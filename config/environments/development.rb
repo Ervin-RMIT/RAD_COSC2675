2ba268d3ea207f568626e62da463322fd0a157e0
Ervin Chua <ervinchua@gmail.com>
2019-05-14 22:32:57 +1000

    Finish 11.3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,8 +29,8 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = '79d37a4d676a428bb50d9a92f8e0da40.vfs.cloud9.ap-southeast-1.amazonaws.com'
-  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
   config.action_mailer.perform_caching = false
 
